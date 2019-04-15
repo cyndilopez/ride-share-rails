82b17b3c9751a6950b74c2c22475881ceebf28d4
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-15 15:52:43 -0700

    added controllers and index actions to controllers and index.html.erb pages for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :drivers, only: [:index]
+
+  resources :passengers, only: [:index]
+
+  resources :trips, only: [:index]
 end
