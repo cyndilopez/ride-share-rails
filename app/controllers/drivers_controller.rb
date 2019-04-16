8cc54028d6cb3c6ae55b2fd27d4db0e9079d63c6
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-15 18:39:07 -0700

    added new, create, and params actions to drivers and passengers controllers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,20 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.all
   end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    driver = Driver.new driver_params
+    driver.save
+    # redirect_to
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,18 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    passenger = Passenger.new passenger_params
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end
