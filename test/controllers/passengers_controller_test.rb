e3ee5a00bdf2728257356378d0c355868bfd75a5
sjscotton <sj.scotton@gmail.com>
2019-04-15 21:08:29 -0700

    Added tests for passenger#show

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,12 +1,25 @@
 require "test_helper"
 
 describe PassengersController do
+  let (:passenger) {
+    Passenger.create name: "sample task", phone_num: "(555) 555-5555"
+  }
   describe "index" do
     # Your tests go here
   end
 
   describe "show" do
-    # Your tests go here
+    it "Can get a task with a valid id" do
+      get passenger_path(passenger.id)
+
+      must_respond_with :ok
+    end
+
+    it "Will redirect if given an invalid Passenger ID" do
+      get passenger_path(-1)
+
+      must_respond_with :redirect
+    end
   end
 
   describe "edit" do
