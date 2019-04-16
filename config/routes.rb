096a0c2e6d6117a53df56c00ae96d15b20cf1f96
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-16 12:00:12 -0700

    routes added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root "homepages#index"
-  resources :drivers, only: [:index, :create, :new]
+  root "homepages#index"
+  resources :drivers, only: [:index, :create, :new, :show]
 
   resources :passengers, only: [:index, :create, :new, :show]
 
