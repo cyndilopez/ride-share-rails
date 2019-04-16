a7b62910b9276799cc0c103abd3032cca997360d
sjscotton <sj.scotton@gmail.com>
2019-04-16 15:17:47 -0700

    added driver average rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,4 +5,10 @@ class Driver < ApplicationRecord
     total = trips.sum { |trip| trip.cost / 100 }
     return total
   end
+
+  def average_rating
+    total = trips.sum { |trip| trip.rating }
+    total /= trips.count.to_f
+    return total.round(2)
+  end
 end
