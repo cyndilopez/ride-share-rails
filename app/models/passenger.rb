8b442993e830067bc0cac4e8cb87c9307607f4fd
sjscotton <sj.scotton@gmail.com>
2019-04-16 15:06:57 -0700

    added passenger total_cost method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,10 @@
 class Passenger < ApplicationRecord
   has_many :trips
 
-  def total_cost
+  def total_charged
+    total = trips.sum { |trip| trip.cost }
+
+    puts total
+    return total / 100
   end
 end
