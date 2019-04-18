a53d865052a85bc0d778b0042b02f380aa88b00e
sjscotton <sj.scotton@gmail.com>
2019-04-18 13:28:01 -0700

    added validation for rating

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,8 @@ class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
 
+  validates :rating, :allow_nil => true, numericality: { greater_than: 0, less_than: 6 }
+
   def self.calc_cost
     cost = rand * 3000 + 200
     return cost.to_i
